c917834bfec6dd3a3895b95abb5943ea9a3fed36
chee <yay@chee.party>
2025-01-28 20:24:22 +0000

    remove build responsibility from vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from "vitest/config"
 import {resolve} from "path"
-import dts from "vite-plugin-dts"
 import wasm from "vite-plugin-wasm"
 import {visualizer} from "rollup-plugin-visualizer"
 import solid from "vite-plugin-solid"
@@ -9,25 +8,5 @@ export default defineConfig({
 	test: {
 		testTimeout: 1000,
 	},
-	plugins: [
-		solid(),
-		wasm(),
-		dts({insertTypesEntry: true}),
-		process.env.VISUALIZE && visualizer(),
-	],
-	build: {
-		outDir: "output",
-		lib: {
-			entry: resolve(__dirname, "src/index.ts"),
-			formats: ["es"],
-			fileName: "index",
-		},
-		target: "esnext",
-		rollupOptions: {
-			external: ["solid-js", "cabbages"],
-		},
-	},
-	worker: {
-		plugins: () => [wasm()],
-	},
+	plugins: [solid(), wasm(), process.env.VISUALIZE && visualizer()],
 })
