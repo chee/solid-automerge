619c39b96a1c70170757959cf0251fff257d6f32
bigmistqke <vandijckv@gmail.com>
2025-01-29 21:45:53 +0100

    split document-projection up in create and use `createDocumentProjection` - expects a ready DocHandle<T> | undefined - returns a store-projection T if passed a DocHandle<T> - returns T | undefined if passed DocHandle<T> | undefined
    `useDocumentProjection`
    - expects a MaybeAccessor<DocHandle<T> | undefined>
    - uses generic to return more narrow return types then MaybeAccessor<T | undefined>

--- src/create-document-projection.ts
+++ src/create-document-projection.ts
@@ -1,10 +1,42 @@
-import {createComputed, onCleanup} from "solid-js"
-import {Doc, DocHandle, DocHandleChangePayload} from "@automerge/automerge-repo"
-import {autoproduce} from "./autoproduce.js"
-import {createStore, reconcile, type Store} from "solid-js/store"
-import {access, type MaybeAccessor} from "@solid-primitives/utils"
+import { DocHandle, DocHandleChangePayload } from "@automerge/automerge-repo"
+import { access, type MaybeAccessor } from "@solid-primitives/utils"
+import { Accessor, createMemo, onCleanup } from "solid-js"
+import { createStore, type Store } from "solid-js/store"
+import { autoproduce } from "./autoproduce.js"
 
 const cache = new WeakMap<DocHandle<unknown>, Store<unknown>>()
+
+type DocFromHandle<T extends DocHandle<unknown>> = NonNullable<ReturnType<T["docSync"]>>
+
+export function createDocumentProjection<T>(handle: DocHandle<T>): T
+export function createDocumentProjection<T>(handle?: DocHandle<T>): T | undefined
+export function createDocumentProjection<T>(handle?: DocHandle<T>) {
+  if (!handle) return undefined
+
+  if (cache.has(handle)) {
+    return cache.get(handle) as Store<T>
+  }
+
+  const initialValue = handle.docSync()
+
+  if (!initialValue) {
+    throw `A document projection can only be created once the handle is ready`
+  }
+
+  const [doc, set] = createStore(initialValue)
+  if (!cache.has(handle)) cache.set(handle, doc)
+
+  function patch(payload: DocHandleChangePayload<T>) {
+    set(autoproduce(payload.patches))
+  }
+
+  handle.on("change", patch)
+  onCleanup(() => {
+    handle.off("change", patch)
+  })
+
+  return doc
+}
 
 /**
  * get a fine-grained live view of a handle's document. it's subscribed to the
@@ -14,37 +46,26 @@ const cache = new WeakMap<DocHandle<unknown>, Store<unknown>>()
  * @param maybeHandle a handle, or a function that returns a handle, or a function
  * that might return a handle one day 
  */
-export function createDocumentProjection<T>(
-	maybeHandle: MaybeAccessor<DocHandle<T> | undefined>
+export function useDocumentProjection<T extends MaybeAccessor<DocHandle<unknown> | undefined>>(
+  maybeHandle: T,
 ) {
-	const handle = access(maybeHandle)
-	if (handle && cache.has(handle)) {
-		return cache.get(handle) as Store<T>
-	}
-	const [doc, set] = createStore<Doc<T>>(handle?.docSync() ?? ({} as T))
-
-	function patch(payload: DocHandleChangePayload<T>) {
-		set(autoproduce(payload.patches))
-	}
-
-	createComputed(() => {
-		const handle = access(maybeHandle)
-		if (handle) {
-			set(handle.docSync() ?? ({} as T))
-			if (!cache.has(handle)) cache.set(handle, doc)
-
-			handle.on("change", patch)
-			onCleanup(() => {
-				handle.off("change", patch)
-			})
-
-			handle.whenReady().then(() => {
-				set(handle.docSync() ?? ({} as T))
-			})
-		} else {
-			set(reconcile({} as T))
-		}
-	})
-
-	return doc
+  if (typeof maybeHandle !== "function") {
+    return createDocumentProjection(access(maybeHandle)) as MaybeDocumentProjection<T>
+  }
+  return createMemo(() => {
+    return createDocumentProjection(access(maybeHandle)) as MaybeDocumentProjection<T>
+  })
 }
+
+type MaybeDocumentProjection<T extends MaybeAccessor<DocHandle<unknown> | undefined>> =
+  T extends () => infer V
+    ? V extends DocHandle<unknown>
+      ? Accessor<DocFromHandle<V>>
+      : V extends undefined
+        ? Accessor<undefined>
+        : never
+    : T extends DocHandle<unknown>
+      ? DocFromHandle<T>
+      : T extends undefined
+        ? undefined
+        : never
