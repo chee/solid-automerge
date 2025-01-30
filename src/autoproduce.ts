5f9b94beb561227fb63dfbd48b0a21af98e5ba96
chee <yay@chee.party>
2025-01-30 23:30:49 +0000

    expect autoproduce to be passed to produce, rather than calling produce itself

--- src/autoproduce.ts
+++ src/autoproduce.ts
@@ -1,21 +1,20 @@
 import type {Patch} from "@automerge/automerge-repo/slim"
 import {apply, fromAutomerge} from "cabbages"
-import {produce} from "solid-js/store"
 
 /**
  * convert automerge patches to solid producer operations
  * @param patches the patches fresh from a
  * [DocHandleChangePayload](https://automerge.org/automerge-repo/interfaces/_automerge_automerge_repo.DocHandleChangePayload.html)
- * @returns a
- * [producer](https://docs.solidjs.com/reference/store-utilities/produce)
- * function to pass to a [Solid
- * Store](https://docs.solidjs.com/reference/store-utilities/create-store)
+ * @returns a callback for an immer-like function. e.g.
+ * [produce](https://docs.solidjs.com/reference/store-utilities/produce) for
+ * [Solid
+ * Stores](https://docs.solidjs.com/reference/store-utilities/create-store)
  */
 export function autoproduce<T>(patches: Patch[]) {
-	return produce<T>(doc => {
+	return (doc: T) => {
 		for (let patch of patches) {
 			const [path, range, val] = fromAutomerge(patch)
 			apply(path, doc, range, val)
 		}
-	})
+	}
 }
