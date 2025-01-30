c820a080bf43868bf072a13db73a55b558526319
chee <yay@chee.party>
2025-01-30 23:25:42 +0000

    remove unused plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import {defineConfig} from "vitest/config"
-import {resolve} from "path"
 import wasm from "vite-plugin-wasm"
-import {visualizer} from "rollup-plugin-visualizer"
 import solid from "vite-plugin-solid"
 
 export default defineConfig({
 	test: {
 		testTimeout: 1000,
 	},
-	plugins: [solid(), wasm(), process.env.VISUALIZE && visualizer()],
+	plugins: [solid(), wasm()],
 })
