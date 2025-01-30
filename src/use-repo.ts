0293107fc6668daf47f5c2140822e721f1841114
chee <yay@chee.party>
2025-01-30 23:35:42 +0000

    update jsdocs for handle and repo

--- src/use-repo.ts
+++ src/use-repo.ts
@@ -3,8 +3,8 @@ import {createContext, useContext} from "solid-js"
 
 /**
  * a [context](https://docs.solidjs.com/concepts/context) that provides access
- * to an Automerge repo. you don't need this, you can pass the repo in the
- * second arg
+ * to an Automerge Repo. you don't need this, you can pass the repo in the
+ * second arg to the functions that need it.
  */
 export const RepoContext = createContext<Repo | null>(null)
 
