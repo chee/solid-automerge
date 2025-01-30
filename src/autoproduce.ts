cf5f854861e7511354df4d66417c6e2727aa9766
chee <yay@chee.party>
2025-01-30 23:26:50 +0000

    add fancy linky docs to autoproduce

--- src/autoproduce.ts
+++ src/autoproduce.ts
@@ -4,8 +4,12 @@ import {produce} from "solid-js/store"
 
 /**
  * convert automerge patches to solid producer operations
- * @param patches the patches fresh from a DocHandleChangePayload
- * @returns a producer function to pass to a solid store
+ * @param patches the patches fresh from a
+ * [DocHandleChangePayload](https://automerge.org/automerge-repo/interfaces/_automerge_automerge_repo.DocHandleChangePayload.html)
+ * @returns a
+ * [producer](https://docs.solidjs.com/reference/store-utilities/produce)
+ * function to pass to a [Solid
+ * Store](https://docs.solidjs.com/reference/store-utilities/create-store)
  */
 export function autoproduce<T>(patches: Patch[]) {
 	return produce<T>(doc => {
