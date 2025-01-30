51e65a48aee41ec2a8790fe0e8b64dd54c3a29c1
chee <yay@chee.party>
2025-01-30 23:34:08 +0000

    Add cleaner createDocumentProjection based on the makeDocumentProjection primitive

--- src/create-document-projection.ts
+++ src/create-document-projection.ts
@@ -1,50 +1,19 @@
-import {createComputed, onCleanup} from "solid-js"
-import {Doc, DocHandle, DocHandleChangePayload} from "@automerge/automerge-repo"
-import {autoproduce} from "./autoproduce.js"
-import {createStore, reconcile, type Store} from "solid-js/store"
-import {access, type MaybeAccessor} from "@solid-primitives/utils"
-
-const cache = new WeakMap<DocHandle<unknown>, Store<unknown>>()
+import {createMemo, type Accessor} from "solid-js"
+import {DocHandle, type Doc} from "@automerge/automerge-repo"
+import {makeDocumentProjection} from "./make-document-projection.js"
+import {access} from "@solid-primitives/utils"
 
 /**
- * get a fine-grained live view of a handle's document. it's subscribed to the
-   handle's changes, and converts incoming automerge operations to solid store
-   updates, providing **fine-grained reactivity that's consistent across space
-   and time**.
- * @param maybeHandle a handle, or a function that returns a handle, or a function
- * that might return a handle one day 
+ * get a fine-grained live view of a document from a handle. works with
+ * {@link useHandle}.
+ * @param handle an accessor (signal/resource) of a
+ * [DocHandle](https://automerge.org/automerge-repo/classes/_automerge_automerge_repo.DocHandle.html)
  */
 export function createDocumentProjection<T>(
-	maybeHandle: MaybeAccessor<DocHandle<T> | undefined>
+	handle: Accessor<DocHandle<T> | undefined>
 ) {
-	const handle = access(maybeHandle)
-	if (handle && cache.has(handle)) {
-		return cache.get(handle) as Store<T>
-	}
-	const [doc, set] = createStore<Doc<T>>(handle?.docSync() ?? ({} as T))
-
-	function patch(payload: DocHandleChangePayload<T>) {
-		set(autoproduce(payload.patches))
-	}
-
-	createComputed(() => {
-		const handle = access(maybeHandle)
-		if (handle) {
-			set(handle.docSync() ?? ({} as T))
-			if (!cache.has(handle)) cache.set(handle, doc)
-
-			handle.on("change", patch)
-			onCleanup(() => {
-				handle.off("change", patch)
-			})
-
-			handle.whenReady().then(() => {
-				set(handle.docSync() ?? ({} as T))
-			})
-		} else {
-			set(reconcile({} as T))
-		}
-	})
-
-	return doc
+	const projection = createMemo<Doc<T> | undefined>(
+		() => access(handle) && makeDocumentProjection<T>(access(handle)!)
+	)
+	return projection
 }
